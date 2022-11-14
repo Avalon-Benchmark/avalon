c7da4faeed83414d54d4c8c6baa0552479075839
Bai Li <bai@generallyintelligent.com>
2022-11-14 22:07:50 +0000

    Merge branch 'bai-evalai-runner' into 'main'
    EvalAI test runner script
    
    This script polls SQS for contest submissions, when a participant makes a submission, it pulls the docker image from ECR, runs it, parses the output, and submits the result to the EvalAI API.
    
    Deployment notes:
    * Need the following environment variables: EVALAI_AWS_ACCESS_KEY_ID, EVALAI_AWS_SECRET_ACCESS_KEY, EVALAI_REGION_NAME, EVALAI_AUTH_TOKEN
    * Need to upgrade our version of docker Python SDK
    
    See merge request generally-intelligent/generally_intelligent!422
    
    Source: af6b793a807cebcbd78dc146521fb062ce5c1472

---
+++ avalon/contest/test_runner/contest_runner_script.py
@@ -0,0 +1,157 @@
+"""
+Script to evaluate participant's submission and report to EvalAI.
+Modified from: https://github.com/Cloud-CV/EvalAI-Starters/blob/master/remote_challenge_evaluation/evaluation_script_starter.py
+"""
+import base64
+import json
+import os
+import re
+import subprocess
+import time
+import urllib.request
+from typing import Any
+from typing import Dict
+
+import boto3
+import docker
+from loguru import logger
+
+from avalon.contest.test_runner.evalai_interface import EvalAI_Interface
+
+
+def login_docker_client_to_aws_ecr() -> None:
+    """
+    Login to ECR, with a workaround for bug in docker SDK.
+    See: https://stackoverflow.com/questions/53759374/docker-login-to-ecr-via-python-docker-sdk
+    """
+    ecr_client = boto3.client(
+        "ecr",
+        aws_access_key_id=os.getenv("EVALAI_AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("EVALAI_AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("EVALAI_REGION_NAME"),
+    )
+
+    token = ecr_client.get_authorization_token()
+    username, password = base64.b64decode(token["authorizationData"][0]["authorizationToken"]).decode().split(":")
+    registry = token["authorizationData"][0]["proxyEndpoint"]
+
+    command = 'docker login -u "%s" -p "%s" "%s"' % (username, password, registry)
+
+    p = subprocess.Popen([command], stdout=subprocess.PIPE, shell=True, bufsize=1)
+    for line in iter(p.stdout.readline, b""):  # type: ignore
+        logger.info(line)
+    p.communicate()  # close p.stdout, wait for the subprocess to exit
+
+
+def handle_submission(evalai: EvalAI_Interface, submission: Dict[str, Any]) -> None:
+    input_file_url = submission["input_file"]
+
+    with urllib.request.urlopen(input_file_url) as url:
+        submitted_image_uri = json.load(url)["submitted_image_uri"]
+
+    repository, tag = submitted_image_uri.split(":")
+    repository = repository.replace("https://", "")
+
+    logger.info("Retrieving image:", submitted_image_uri)
+    logger.info("Repository:", repository)
+    logger.info("Tag:", tag)
+
+    login_docker_client_to_aws_ecr()
+
+    docker_client = docker.from_env()
+    submitted_image = docker_client.images.pull(repository=repository, tag=tag)
+
+    # Update EvalAI right after sending the submission into "RUNNING" state
+    evalai.update_submission_status({"submission": submission["id"], "submission_status": "RUNNING"})
+
+    try:
+        logger.info(f"Running submission: {submitted_image_uri}")
+
+        container_logs = docker_client.containers.run(
+            submitted_image,
+            device_requests=[docker.types.DeviceRequest(count=-1, capabilities=[["compute", "utility", "graphics"]])],
+        )
+
+        logger.info(f"Finished running submission. Output: {container_logs}")
+
+        # Parse submission score from container stdout
+        regex_result = re.search(r"TEST RESULTS\\n({.*})", str(container_logs))
+        if regex_result is None:
+            raise Exception("Submission did not output any test results")
+
+        result_json = regex_result.group(1)
+        result_json = json.loads(result_json.replace("'", '"'))
+
+        # Todo: set up proper phases, data splits, and metrics in contest config.
+        submission_data = [
+            {
+                "split": "train_split",
+                "show_to_participant": True,
+                "accuracies": {"Total": result_json["overall_success_rate"]},
+            }
+        ]
+
+        # Update EvalAI after calculating final set of metrics and set submission status as "FINISHED"
+        evalai.update_submission_data(
+            {
+                "challenge_phase": submission["challenge_phase"],
+                "submission": submission["id"],
+                "stdout": container_logs,
+                "submission_status": "FINISHED",
+                "result": json.dumps(submission_data),
+            }
+        )
+
+        logger.info(f"Reported {submission['id']} to EvalAI: {submission_data}")
+
+    except Exception as e:
+        # Update EvalAI in case of errors and set submission status as "FAILED"
+        evalai.update_submission_data(
+            {
+                "challenge_phase": submission["challenge_phase"],
+                "submission": submission["id"],
+                "submission_status": "FAILED",
+            }
+        )
+
+        logger.error(f"Failed to run submitted image: {e}")
+        raise
+
+
+if __name__ == "__main__":
+    auth_token = os.getenv("EVALAI_AUTH_TOKEN")
+    evalai_api_server = "https://eval.ai"
+    queue_name = "random-number-generator-challenge-1882-production-3364fa36-c6a1-47b5-a7b4-2e6400"
+    challenge_pk = "1882"
+
+    evalai = EvalAI_Interface(auth_token, evalai_api_server, queue_name, challenge_pk)
+
+    while True:
+        # Get the message from the queue
+        message = evalai.get_message_from_sqs_queue()
+        message_body = message.get("body")
+        if message_body:
+            submission_pk = message_body.get("submission_pk")
+            challenge_pk = message_body.get("challenge_pk")
+            phase_pk = message_body.get("phase_pk")
+            # Get submission details -- This will contain the input file URL
+            submission: Dict[str, Any] = evalai.get_submission_by_pk(submission_pk)
+
+            if (
+                submission.get("status") == "finished"
+                or submission.get("status") == "failed"
+                or submission.get("status") == "cancelled"
+            ):
+                message_receipt_handle = message.get("receipt_handle")
+                evalai.delete_message_from_sqs_queue(message_receipt_handle)
+
+            elif submission.get("status") == "running":
+                # Do nothing on EvalAI
+                pass
+
+            else:
+                handle_submission(evalai, submission)
+
+        # Poll challenge queue for new submissions
+        logger.info("Waiting for submission...")
+        time.sleep(30)
---
+++ avalon/contest/test_runner/evalai_interface.py
@@ -0,0 +1,141 @@
+""""
+Client interface for EvalAI.
+Copied mostly without modification from:
+https://github.com/Cloud-CV/EvalAI-Starters/blob/master/remote_challenge_evaluation/evaluation_script_starter.py
+"""
+import requests
+from loguru import logger
+
+URLS = {
+    "get_message_from_sqs_queue": "/api/jobs/challenge/queues/{}/",
+    "get_submission_by_pk": "/api/jobs/submission/{}",
+    "delete_message_from_sqs_queue": "/api/jobs/queues/{}/",
+    "update_submission": "/api/jobs/challenge/{}/update_submission/",
+}
+
+
+class EvalAI_Interface:
+    def __init__(self, AUTH_TOKEN, EVALAI_API_SERVER, QUEUE_NAME, CHALLENGE_PK):  # type: ignore
+        """Class to initiate call to EvalAI backend
+
+        Arguments:
+            AUTH_TOKEN {[string]} -- The authentication token corresponding to EvalAI
+            EVALAI_API_SERVER {[string]} -- It should be set to https://eval.ai # For production server
+            QUEUE_NAME {[string]} -- Unique queue name corresponding to every challenge
+            CHALLENGE_PK {[integer]} -- Primary key corresponding to a challenge
+        """
+
+        self.AUTH_TOKEN = AUTH_TOKEN
+        self.EVALAI_API_SERVER = EVALAI_API_SERVER
+        self.QUEUE_NAME = QUEUE_NAME
+        self.CHALLENGE_PK = CHALLENGE_PK
+
+    def get_request_headers(self):  # type: ignore
+        """Function to get the header of the EvalAI request in proper format
+
+        Returns:
+            [dict]: Authorization header
+        """
+        headers = {"Authorization": "Bearer {}".format(self.AUTH_TOKEN)}
+        return headers
+
+    def make_request(self, url, method, data=None):  # type: ignore
+        """Function to make request to EvalAI interface
+
+        Args:
+            url ([str]): URL of the request
+            method ([str]): Method of the request
+            data ([dict], optional): Data of the request. Defaults to None.
+
+        Returns:
+            [JSON]: JSON response data
+        """
+        headers = self.get_request_headers()
+        try:
+            response = requests.request(method=method, url=url, headers=headers, data=data)
+            response.raise_for_status()
+        except requests.exceptions.RequestException:
+            logger.info("The server isn't able establish connection with EvalAI")
+            raise
+        return response.json()
+
+    def return_url_per_environment(self, url):  # type: ignore
+        """Function to get the URL for API
+
+        Args:
+            url ([str]): API endpoint url to which the request is to be made
+
+        Returns:
+            [str]: API endpoint url with EvalAI base url attached
+        """
+        base_url = "{0}".format(self.EVALAI_API_SERVER)
+        url = "{0}{1}".format(base_url, url)
+        return url
+
+    def get_message_from_sqs_queue(self):  # type: ignore
+        """Function to get the message from SQS Queue
+
+        Docs: https://eval.ai/api/docs/#operation/get_submission_message_from_queue
+
+        Returns:
+            [JSON]: JSON response data
+        """
+        url = URLS.get("get_message_from_sqs_queue").format(self.QUEUE_NAME)
+        url = self.return_url_per_environment(url)
+        response = self.make_request(url, "GET")
+        return response
+
+    def delete_message_from_sqs_queue(self, receipt_handle):  # type: ignore
+        """Function to delete the submission message from the queue
+
+        Docs: https://eval.ai/api/docs/#operation/delete_submission_message_from_queue
+
+        Args:
+            receipt_handle ([str]): Receipt handle of the message to be deleted
+
+        Returns:
+            [JSON]: JSON response data
+        """
+        url = URLS.get("delete_message_from_sqs_queue").format(self.QUEUE_NAME)
+        url = self.return_url_per_environment(url)
+        data = {"receipt_handle": receipt_handle}
+        response = self.make_request(url, "POST", data)
+        return response
+
+    def update_submission_data(self, data):  # type: ignore
+        """Function to update the submission data on EvalAI
+
+        Docs: https://eval.ai/api/docs/#operation/update_submission
+
+        Args:
+            data ([dict]): Data to be updated
+
+        Returns:
+            [JSON]: JSON response data
+        """
+        url = URLS.get("update_submission").format(self.CHALLENGE_PK)
+        url = self.return_url_per_environment(url)
+        response = self.make_request(url, "PUT", data=data)
+        return response
+
+    def update_submission_status(self, data):  # type: ignore
+        """
+
+        Docs: https://eval.ai/api/docs/#operation/update_submission
+
+        Args:
+            data ([dict]): Data to be updated
+
+        Returns:
+            [JSON]: JSON response data
+        """
+        url = URLS.get("update_submission").format(self.CHALLENGE_PK)
+        url = self.return_url_per_environment(url)
+        response = self.make_request(url, "PATCH", data=data)
+        return response
+
+    def get_submission_by_pk(self, submission_pk):  # type: ignore
+        url = URLS.get("get_submission_by_pk").format(submission_pk)
+        url = self.return_url_per_environment(url)
+        response = self.make_request(url, "GET")
+        return response
