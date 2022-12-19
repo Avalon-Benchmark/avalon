a1abed6af0d85a32e965c7b8e5f481f985df4f01
Zack Polizzi <zplizzi@gmail.com>
2022-12-19 14:28:29 +0000

    Merge branch 'zack/fix_check_install' into 'main'
    fix check install on mac
    
    Mac uses `spawn` mode multiprocessing which requires this guard.
    
    See merge request generally-intelligent/generally_intelligent!446
    
    Source: fee139517a63dd3281ad9c19c2f8e2ebf2f13ce2

--- avalon/common/check_install.py
+++ avalon/common/check_install.py
@@ -4,25 +4,24 @@ from avalon.agent.godot.godot_gym import GodotEnvironmentParams
 from avalon.agent.godot.godot_gym import TrainingProtocolChoice
 from avalon.common.log_utils import configure_local_logger
 
-configure_local_logger()
+if __name__ == "__main__":
+    configure_local_logger()
 
-env_params = GodotEnvironmentParams(
-    resolution=96,
-    training_protocol=TrainingProtocolChoice.SINGLE_TASK_EAT,
-    initial_difficulty=0,
-)
-env = AvalonEnv(env_params)
-env.reset()
+    env_params = GodotEnvironmentParams(
+        resolution=96,
+        training_protocol=TrainingProtocolChoice.SINGLE_TASK_EAT,
+        initial_difficulty=0,
+    )
+    env = AvalonEnv(env_params)
+    env.reset()
 
+    def random_env_step():
+        action = env.action_space.sample()
+        obs, reward, done, info = env.step(action)
+        if done:
+            env.reset()
+        return obs
 
-def random_env_step():
-    action = env.action_space.sample()
-    obs, reward, done, info = env.step(action)
-    if done:
-        env.reset()
-    return obs
+    observations = [random_env_step() for _ in range(50)]
 
-
-observations = [random_env_step() for _ in range(50)]
-
-print("avalon seems to be working properly!")  # script
+    print("avalon seems to be working properly!")  # script
