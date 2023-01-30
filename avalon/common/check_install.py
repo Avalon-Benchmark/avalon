7b7c39639e2f2349a0b204d695c40a5c14ca2310
Zack Polizzi <zplizzi@gmail.com>
2023-01-30 19:55:15 +0000

    Merge branch 'zack/cleanup_check_install' into 'main'
    cleanup avalon check install
    
    Moves code into a separate function as josh had previously suggested, and also adds an `env.close()` because someone noted in an issue that the cleanup was throwing an error - hopefully this will resolve that.
    
    Tested running the script on my mac.
    
    See merge request generally-intelligent/generally_intelligent!481
    
    Source: c7876f787c44965b2e217eec7f287bc84aab4a70

--- avalon/common/check_install.py
+++ avalon/common/check_install.py
@@ -1,12 +1,13 @@
 """A script that exercises the basic Avalon functionality to ensure the install was successful."""
+from loguru import logger
+
 from avalon.agent.godot.godot_gym import AvalonEnv
 from avalon.agent.godot.godot_gym import GodotEnvironmentParams
 from avalon.agent.godot.godot_gym import TrainingProtocolChoice
 from avalon.common.log_utils import configure_local_logger
 
-if __name__ == "__main__":
-    configure_local_logger()
 
+def check_install() -> None:
     env_params = GodotEnvironmentParams(
         resolution=96,
         training_protocol=TrainingProtocolChoice.SINGLE_TASK_EAT,
@@ -24,4 +25,10 @@ if __name__ == "__main__":
 
     observations = [random_env_step() for _ in range(50)]
 
-    print("avalon seems to be working properly!")  # script
+    logger.info("avalon seems to be working properly!")
+    env.close()
+
+
+if __name__ == "__main__":
+    configure_local_logger()
+    check_install()
